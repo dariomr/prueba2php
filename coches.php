84c001f68077b022ab2ada689bf23c815edef8e7
Darío <210188@fppiramide.com>
2021-10-08 10:18:49 +0200

    added new Coche entity

--- coches.php
+++ coches.php
@@ -3,5 +3,6 @@
 $arrayCoches = array(
     new Coche('123', 'Opel', 'Corsa', '1000000'),
     new Coche('456', 'Renault', 'Megane', '5000'),
-    new Coche('789', 'Ford', 'Focus', '25000')
+    new Coche('789', 'Ford', 'Focus', '25000'),
+    new Coche('453', 'Seat', 'Ibiza', '50000')
 );
